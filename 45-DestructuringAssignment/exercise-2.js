53666e693199f5417f12174c3f5aa27d04603962
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-08-29 17:33:22 +0200

    Esercizio 45

---
+++ 45-DestructuringAssignment/exercise-2.js
@@ -0,0 +1,15 @@
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+let { id, firstName, lastName, age } = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+console.log(id, firstName, lastName, age);
